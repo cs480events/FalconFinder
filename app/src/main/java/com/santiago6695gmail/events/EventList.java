cb170047830c017a0e9696be9d4611158a9cb1c9
tehpieguy <leonard_thom@bentley.edu>
2016-04-27 21:28:26 -0400

    events are completely functional

--- app/src/main/java/com/santiago6695gmail/events/EventList.java
+++ app/src/main/java/com/santiago6695gmail/events/EventList.java
@@ -73,7 +73,6 @@ public class EventList extends Activity implements AdapterView.OnItemClickListen
         return true;
     }
     public void goCategory (MenuItem item) {
-        setContentView(R.layout.category_list);
         Intent i = new Intent(this, Category.class);
         startActivity(i);
     }
