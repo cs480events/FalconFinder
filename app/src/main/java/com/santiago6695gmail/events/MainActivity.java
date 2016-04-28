e8c8487929af40977db4bead48672f25a55bb053
cs480events <Waterfall#2016>
2016-04-28 04:34:19 -0400

    main activity comments

--- app/src/main/java/com/santiago6695gmail/events/MainActivity.java
+++ app/src/main/java/com/santiago6695gmail/events/MainActivity.java
@@ -22,27 +22,25 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity implements OnClickListener {
-    public static String emailFieldString = "marat_dula@bentley.edu";
-    public static EditText emailField;
-    public static String dumy;
-    private EditText passwordField;
-    private Button signInButton;
-    private TextView userEmail;
-    private TextView userPassword;
+
+    public static String emailFieldString = "marat_dula@bentley.edu";// hardcoded string until global variable works
+    public static EditText emailField; // where user enters email
+//    public static String emailFieldString = emailField.getText().toString();// testing global variable
+    public static String dumy; // dumy string to think
+    private EditText passwordField; // where user enters password
+    private Button signInButton; // sign in button
+    private TextView userEmail; // text view of user email
+    private TextView userPassword; // text view of user password
     public Intent i;
 
-    private Thread t = null;
-    private ArrayList<String> list = new ArrayList<String>();
-    private TextView slogan = null;
-    private Button webLogIn;
-    private Button admindial;
+    private Thread t = null; //variable for thread
+    private TextView slogan = null; // Textview for slogan animal
+    private Button webLogIn; // web login button
+    private Button admindial; // dialer button to call
 
-    private String password;
-    private static final String tag = "Usernames: ";
-
-    public boolean flag = false;
-    public String email;
-    String testingUser;
+    private String password; // string to hold the password
+    private static final String tag = "Usernames: "; // unviversal tag to check usernames
+    public String email; // using to store the email of the user as a string
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,6 +61,7 @@ public class MainActivity extends Activity implements OnClickListener {
         slogan.startAnimation(in);
         slogan.setText("Find all events on campus!");
 
+        // setting up the text view and edit text views for user to enter
         emailField = (EditText) findViewById(R.id.enterEmail);
         passwordField = (EditText) findViewById(R.id.enterPassword);
 
@@ -77,6 +76,7 @@ public class MainActivity extends Activity implements OnClickListener {
             @Override
             public void onClick(View v) {
 
+                // dialer to call a cell phone for our teammate
                 Uri dialuri = Uri.parse("tel:6039217921");
                 Intent dialintent = new Intent(Intent.ACTION_CALL, dialuri);
                 startActivity(dialintent); //Has a red line underneath, but runs completely normal
@@ -84,10 +84,10 @@ public class MainActivity extends Activity implements OnClickListener {
         });
 
 
-
+        // intent to declare to it wil move on to another class
         i = new Intent (this, Category.class);
 
-
+        //button for web browser
         webLogIn = (Button) findViewById(R.id.log_in_web);
         webLogIn.setOnClickListener(new OnClickListener() {
             @Override
@@ -98,23 +98,24 @@ public class MainActivity extends Activity implements OnClickListener {
 
 
     }
+    //opeing browser intent
     public void openBrowser() {
         Uri uri = Uri.parse("http://frodo.bentley.edu/CS460Teamc");
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);
     }
-
+    // clikck on an object
     public void onClick(View v)
     {
         switch (v.getId()) {
 
-            case R.id.signin:
+            case R.id.signin: // sign in button click
             {
 
-                emailFieldString = emailField.getText().toString();
-                Log.e("Checking",emailFieldString);
-                t = new Thread(background);
-                t.start();
+                emailFieldString = emailField.getText().toString(); // set email field to email field string
+                Log.e("Checking",emailFieldString); // writing to the log checking if it got the username's email
+                t = new Thread(background); // making a thread
+                t.start(); // starting a thread
             }
 
         }
@@ -122,6 +123,7 @@ public class MainActivity extends Activity implements OnClickListener {
     private Runnable background = new Runnable()
     {
         public void run(){
+            //Database credentials and url for easuer use
             String URL = "jdbc:mysql://frodo.bentley.edu:3306/CS460Teamc";
             String username = "cs460teamc";
             String password = "cs460teamc";
@@ -145,39 +147,33 @@ public class MainActivity extends Activity implements OnClickListener {
             }
 
             try {
-                //String mysql = "select password from cs460teamc.user WHERE email ="+testing +")";
-                //Log.e("Hey", mysql);
-//                ResultSet result = stmt.executeQuery(mysql);
-//                ResultSet result = stmt.executeQuery("select password from cs460teamc.user WHERE email = 'XIE_XIAO@bentley.edu';");
-
+               //executing the statement in the sql database
                 ResultSet result = stmt.executeQuery("select password from cs460teamc.user WHERE email = '"+emailFieldString.trim()+"';");
-                Log.e("LOGGINGIN",emailFieldString);
-                email = emailFieldString;
+                Log.e("LOGGINGIN",emailFieldString); // writing to the log loggining with username
+                email = emailFieldString; //storing variable for use of global variable
                 while (result.next())
                 {
-                    //Read all usernames into one string
-
-                    password = result.getString("password");
-                    Log.e("pass",password);
+                    password = result.getString("password"); // set password
+                    Log.e("pass",password); //logg the password what is it encrypted version
                 }
-                char[] ePassword = password.toCharArray();
-                ePassword[2] = 'a';
-                password = String.valueOf(ePassword);
+                char[] ePassword = password.toCharArray(); // get the encrypted password and set character to array
+                ePassword[2] = 'a'; // get the third character in the array a replace with "a" insted of "y" for decryption
+                password = String.valueOf(ePassword); // set the password a string
 
-                if(BCrypt.checkpw(passwordField.getText().toString().trim(), password))
+                if(BCrypt.checkpw(passwordField.getText().toString().trim(), password)) // if the encrypted password from database equals the encyrpted password of user
                 {
-                    Log.e("M", "True");
+                    Log.e("M", "True"); // if the password match log it and start activity
                     startActivity(i);
                 }
                 else
                 {
-                    Log.e("M","False");
+                    Log.e("M","False"); // if ecnryption passwords don't match
                 }
-                Log.e("JDBC TEST", password);
+                Log.e("JDBC TEST", password); // see which password was used
 
                 //clean up
                 //t = null;
-            } catch (SQLException e) {
+            } catch (SQLException e) { // catch
                 Log.e("JDBC","problems with SQL sent to "+URL+
                         ": "+e.getMessage());
             }finally {
