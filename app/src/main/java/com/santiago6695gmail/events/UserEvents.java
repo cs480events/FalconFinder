0cede5228134beb9111cfece608ef568d943cb2e
tehpieguy <leonard_thom@bentley.edu>
2016-05-04 12:54:02 -0400

    changes

--- app/src/main/java/com/santiago6695gmail/events/UserEvents.java
+++ app/src/main/java/com/santiago6695gmail/events/UserEvents.java
@@ -145,8 +145,8 @@ public class UserEvents extends Activity implements AdapterView.OnItemClickListe
                         String eventdate = result.getString("date"); //event date
                         String eventtime = result.getString("start_time"); //event start time
                         String eventID = result.getString("EventID"); //primary key, ABSOLUTELY NECCESARY
-                        String finalevents = " " + eventnames + "  " + "\n" + "\n" + "\n" + "   " +
-                                eventloc + "    ||    " + eventdate + "    ||    " + eventtime + "\n"; //format for the user's pleasure
+                        String finalevents = " " + eventnames + "  " + "\n" + "\n"  +
+                            eventloc + "\n" + eventdate + "\n" + eventtime + "\n"; //format for the user's pleasure
 
                         items.add(finalevents); //add the formatted string to the list view
 
